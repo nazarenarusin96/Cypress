2817f21a389b726a1d4d1c52c759ce5f4d47f48b
Nazarena Rusin <rusinnazarenaf@gmail.com>
2022-12-05 15:56:45 -0300

    Se implementa la funcion de Pages Objects en el caso CP003 y se optimiza el codigo

--- cypress/e2e/CP003.cy.js
+++ cypress/e2e/CP003.cy.js
@@ -1,27 +1,25 @@
+import indexPage from "../support/index";
+
 describe('empty spec', () => {
   it('Visitar tienda Movistar', () => {
     cy.visit('https://www.movistar.com.ar');
   });
 
   it('Ingresar al tercer equipo de la lista inicial', () => {
-    cy.get('.js-device-caruosel-auto > .slick-list > .slick-track > [data-slick-index="2"]').click({force:true});
+    indexPage.lista();
   });
 
-  
   it('Ver opciones de financiacion', () => {
-    cy.get('#open-installments-modal').click({force:true});
+    indexPage.financiacion();
   });
 
-  it('Elegir el banco Credicoop', () => {
-    cy.get('#selectBank').select("Credicoop");
-  });
-
-  it('Elegir tarjeta VISA', () => {
-    cy.get('#selectCardByBank').select("Visa");
-  });
 
   it('Verifica que no exista el metodo de pago de 60 cuotas', () => {
-    cy.contains('60 Cuotas').should("not.exist");
+    indexPage.validacion();
+  });
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
   });
   
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,13 +1,25 @@
 class indexPage{
     constructor(){
+
+        //CP001
+
         this.verEquipos=".device-carousel-title > a",
         this.search="#search",
         this.seleccionarSearch="#search_mini_form",
         this.seleccionarEquipo="#qs-option-0",
         this.ingresarEquipo=".product-image-photo",
         this.verificacion=".active > .details > ul > :nth-child(1)"
+
+        //CP003
+
+        this.terceroLista='.js-device-caruosel-auto > .slick-list > .slick-track > [data-slick-index="2"]',
+        this.opFinanciacion='#open-installments-modal',
+        this.bancoCredicoop='#selectBank',
+        this.tarjetaVisa='#selectCardByBank',
+        this.verificaNoExist='60 Cuotas'
     }
 
+    //CP001
     verLista=(element)=>{
         cy.log("Ingresa a la lista de equipos");
         cy.get(this.verEquipos).click();
@@ -22,5 +34,21 @@ class indexPage{
 
     verification=(element)=>{
         cy.get(this.verificacion).contains("12");
+    }
+
+    //CP003
+
+    lista=(element)=>{
+        cy.get(this.terceroLista).click({force:true});
+    }
+
+    financiacion=(element)=>{
+        cy.get(this.opFinanciacion).click({force:true});
+        cy.get(this.bancoCredicoop).select("Credicoop");
+        cy.get(this.tarjetaVisa).select("Visa");
+    }
+
+    validacion=(element)=>{
+        cy.contains(this.verificaNoExist).should("not.exist");
     }
 }export default new indexPage();
