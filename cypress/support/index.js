e104b85a298a4aeb9e52c22ee549738e9e26b377
Nazarena Rusin <rusinnazarenaf@gmail.com>
2022-12-05 15:32:25 -0300

    Se agregaron funcionalidades con Pages Objects y se optimizo el codigo del caso CP001

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,26 @@
 class indexPage{
     constructor(){
-        this.verEquipos=".device-carousel-title > a"
+        this.verEquipos=".device-carousel-title > a",
+        this.search="#search",
+        this.seleccionarSearch="#search_mini_form",
+        this.seleccionarEquipo="#qs-option-0",
+        this.ingresarEquipo=".product-image-photo",
+        this.verificacion=".active > .details > ul > :nth-child(1)"
     }
 
-    search=(element)=>{
-        cy.get(this.verEquipos).click({force:true});
+    verLista=(element)=>{
+        cy.log("Ingresa a la lista de equipos");
+        cy.get(this.verEquipos).click();
+    }
+
+    selectSearch=(element)=>{
+        cy.get(this.search).click({force:true}).type("a52s");
+        cy.get(this.seleccionarSearch).click({force:true});
+        cy.get(this.seleccionarEquipo).click({force:true});
+        cy.get(this.ingresarEquipo).click({force:true});
+    }
+
+    verification=(element)=>{
+        cy.get(this.verificacion).contains("12");
     }
 }export default new indexPage();
